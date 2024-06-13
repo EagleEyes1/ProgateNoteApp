60534096957346e9d9cddd529ba1535a7488fd12
unknown <fahder7@gmail.com>
2024-06-13 12:51:08 +0700

    done making noteapp

---
+++ src/screens/home.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { FlatList, StyleSheet, View, Text } from 'react-native'
+import CustomButton from '../components/customButton'
+
+const NoteCard = ({ item, setCurrentPage, setEditNoteId, deleteNote }) => (
+  <>
+  <View style={styles.card}>
+    <Text style={styles.cardTitle}>{item.title}</Text>
+    <Text>{item.desc}</Text>
+    <View style={styles.buttons}>
+      <CustomButton
+        backgroundColor="#FFC300"
+        color="#151D3B"
+        text="Ubah"
+        fontSize={12}
+        width={100}
+        // Tuliskan layar "edit" untuk ketika tombol-nya ditekan
+        onPress={() => {
+            setEditNoteId(item.id)
+          setCurrentPage('edit')
+        }}
+      />
+      <CustomButton
+        backgroundColor="#D82148"
+        color="#fff"
+        text="Hapus"
+        fontSize={12}
+        width={100}
+        onPress={() => deleteNote(item.id)}
+      />
+    </View>
+  </View>
+  </>
+)
+
+const Home = ({ noteList, setCurrentPage, setEditNoteId, deleteNote }) => (
+  <>
+  <View style={styles.container}>
+    <CustomButton
+      backgroundColor="#DDD"
+      color="#203239"
+      text="Tambahkan Note"
+      width="100%"
+      // Tuliskan layar "add" untuk ketika tombol-nya ditekan
+      onPress={() => {
+        setCurrentPage('add')
+      }}
+    />
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      data={noteList}
+      // Berikan function "setCurrentPage" ke component "NoteCard"
+      renderItem={({ item }) => (
+        <NoteCard
+        item={item}
+        setCurrentPage={setCurrentPage}
+        setEditNoteId={setEditNoteId}
+        deleteNote={deleteNote}
+        />
+      )}
+      keyExtractor={(item) => item.id}
+    />
+  </View>
+  <View>
+      <Text style={styles.footer}>Made By Fahd Erlangga B.M</Text>
+   </View>
+   </>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 20,
+    marginTop:50,
+  },
+  card: {
+    padding: 10,
+    marginVertical: 10,
+    borderColor: '#DDD',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  cardTitle: {
+    fontWeight: '600',
+    color: '#203239',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  buttons: {
+    marginTop: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  footer: {
+    textAlign:"right",
+    color:"grey",
+    paddingRight:25,
+  }
+})
+
+export default Home
